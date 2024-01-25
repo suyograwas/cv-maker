c2ab8e8e72043cdbc384f4a795173ce0e038a20a
suyograwas <suyograwas4@gmail.com>
2024-01-26 02:01:20 +0530

    completed the  design of authentication page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        txtPrimary: "#555",
+        txtLight: "#999",
+        txtDark: "#222",
+        bgPrimary: "#f1f1f1",
+      },
+    },
   },
   plugins: [],
 };
